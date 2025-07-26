b2d9d769db0a2658db3aa4e4dd03f9da05069065
MMarzoo <mmarzouk12378@gmail.com>
2025-07-26 18:26:34 +0300

    Day1 Tasks

---
+++ Lab1/script.js
@@ -0,0 +1,64 @@
+//get meal data from the API and display it on the page
+function getMeal(type) {
+  showLoader();
+
+  var myRequest = new XMLHttpRequest();
+  myRequest.open(
+    "GET",
+    `https://forkify-api.herokuapp.com/api/v2/recipes?search=${type}`
+  );
+  myRequest.send();
+
+  myRequest.addEventListener("readystatechange", function () {
+    if (myRequest.readyState === 4) {
+      hideLoader();
+
+      if (myRequest.status === 200) {
+        var data = JSON.parse(myRequest.response).data.recipes;
+        display(data);
+      } else if (myRequest.status === 404) {
+        document.querySelector(
+          "#card-container"
+        ).innerHTML = `<p>No meals found for "${type}".</p>`;
+      }
+    }
+  });
+}
+
+// Function to display the meal data in cards
+function display(list) {
+  var container = "";
+  for (var i = 0; i < list.length; i++) {
+    container += `
+      <div class="card">
+        <img src="${list[i].image_url}" alt="Meal Image" />
+        <h3>${list[i].title}</h3>
+        <p>Publisher: ${list[i].publisher}</p>
+      </div>`;
+  }
+
+  document.querySelector("#card-container").innerHTML = container;
+}
+
+window.onload = function () {
+  getMeal("pasta");
+};
+
+// Add event listeners to buttons in the navbar
+var btns = document.querySelectorAll(".navbar button");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    var type = btn.getAttribute("data-type");
+    getMeal(type);
+  });
+});
+
+//function to show and hide the loader(spinner)
+function showLoader() {
+  document.getElementById("loader").style.display = "block";
+}
+
+function hideLoader() {
+  document.getElementById("loader").style.display = "none";
+}
